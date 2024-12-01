1ee54bab0c4c8846f18f478207acbfdcd77dd4ec
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-12-02 01:29:21 +0530

    Update azureLogin.groovy

--- src/utils/azureLogin.groovy
+++ src/utils/azureLogin.groovy
@@ -24,7 +24,7 @@ def azLogin(String azureCredentials, String environment) {
 
                 // Perform Azure login with the service principal credentials
                 sh """
-                    az login --service-principal --username ${USERNAME} --password ${PASSWORD} --tenant ${TENANT_ID} --use-device-code
+                    az login --service-principal --username ${USERNAME} --password ${PASSWORD} --tenant ${TENANT_ID}
                 """
                 echo "Login successful."    
             
